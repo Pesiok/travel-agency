e18230279e748522c8248695d525a7077234fef0
Grzegorz Bielski <pesiok@gmail.com>
2017-01-08 17:04:13 +0100

    Completed 2 blocks

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+//Watch
+gulp.task('watch', function() {
+    
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+    
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+    //watch every file in folder/future subfolders that have .css extension
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+    
+});
+
+//dependencies in arrays will be run first
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+    
+});
